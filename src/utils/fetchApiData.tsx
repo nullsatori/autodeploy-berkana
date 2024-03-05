d80d947b4104580f6721b61ee0ac08ca23128ba2
satori <nullsatori@gmail.com>
2024-03-05 08:51:27 +0300

    fixes

--- src/utils/fetchApiData.tsx
+++ src/utils/fetchApiData.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiUrl = "http://localhost:1337/api"; // Replace with your Strapi server URL
 const apiToken =
-  "a80179440035dfc045309e39ee2e72d186dad3f0a1b3d52087ec6195613dfe49ff0b03f17c58bd6bcd76fcd794dbbe1e15ff3d162016ba128606d752d1b033e50efd5a175df027f134fe2da8a1ac7254b60ed36e990d511031e72c0e62c5436d24685252e8a638e02fac530c7ade4a46afb6d91767a873eb07b156b5509de683"; // Replace with the API token you generated
+  "60d1a28c385a630993fecb3a7064fc90530f31f24cbc706cc1f1d11b6ce4d352db8c98b6de5535eb45a77234e09eeea5861b89a724773524ac9c16682951057b3b5a8811ead65dab73aee2ae2e99a141a7c1cb487bd2c0b74ef2a098e82049ee0ed1e4d6d341b7c8c952abe453db8ee43c25f7c0691ef62eb3610921b5af63e5"; // Replace with the API token you generated
 
 const fetchApiData = async (endpoint: string) => {
   try {
