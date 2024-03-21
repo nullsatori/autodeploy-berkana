ecef8a64dd88078ae212661a235ff4ed22d29faf
satori <nullsatori@gmail.com>
2024-03-21 20:22:57 +0000

    test commit from server

--- src/utils/fetchApiData.tsx
+++ src/utils/fetchApiData.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const apiUrl = "http://localhost:1337/api"; // Replace with your Strapi server URL
-const apiToken =
-  "60d1a28c385a630993fecb3a7064fc90530f31f24cbc706cc1f1d11b6ce4d352db8c98b6de5535eb45a77234e09eeea5861b89a724773524ac9c16682951057b3b5a8811ead65dab73aee2ae2e99a141a7c1cb487bd2c0b74ef2a098e82049ee0ed1e4d6d341b7c8c952abe453db8ee43c25f7c0691ef62eb3610921b5af63e5"; // Replace with the API token you generated
+const apiUrl = "https://admin.berkanabio.ru/api"; // Replace with your Strapi server URL
+const apiToken = "8c7e55cb62137310af8c2c8d1b44cc1f96117957315d9ff35a62b6836e78728a54cb999772273fc0f2e824d0abc52df1ab90ede4f2f8417008a2e3c2c07433ee4156d1f9b4a436cd18810045719b4b0b242f7d0ec79e148462bc51e2c2da968d63f5df4caf88998bf0bd5619d360328e949bfee564d71d9c2ba73c84987fff47"
 
 const fetchApiData = async (endpoint: string) => {
   try {
