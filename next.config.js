193eb7819920ec4bf0643eedab0830e1ac6544e3
satori <nullsatori@gmail.com>
2024-04-01 20:56:03 +0300

    changed redirect and contacts page

--- next.config.js
+++ next.config.js
@@ -1,29 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-     config.module.rules.push({
-       test: /\.json$/,
-       type: "javascript/auto",
-       use: [
-         {
-           loader: "json-loader",
-         },
-       ],
-     });
- 
-     return config;
-  },
-  async redirects() {
-     return [
-       {
-         source: "/((?!maintenance).*)",
-         destination: "/maintenance",
-         permanent: false,
-       },
-     ];
-  },
- };
- 
- module.exports = nextConfig;
- 
+ reactStrictMode: true,
+ webpack: (config) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: "javascript/auto",
+      use: [
+        {
+          loader: "json-loader",
+        },
+      ],
+    });
+
+    return config;
+ },
+ async redirects() {
+    return [
+      {
+        source: "/((?!contacts).*)",
+        destination: "/contacts",
+        permanent: false,
+      },
+    ];
+ },
+};
+
+module.exports = nextConfig;
