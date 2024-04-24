363ee0361a16daea901a797415e47463c318c1c3
satori <nullsatori@gmail.com>
2024-04-24 14:53:15 +0300

    removed leftover partners block

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,22 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import BackgroundSlider from "@/components/index-page/background-slider";
 import About from "@/components/index-page/about";
 import Video from "@/components/index-page/video";
 import Prices from "@/components/index-page/prices";
-import PartnersBlock from "@/components/index-page/partners-block";
 import AgroVideo from "@/components/index-page/agro-video";
 import Layout from "@/components/general/layout";
-import Events from "@/components/index-page/events";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export default function Home() {
-  const scrolltoHash = function (element_id: string) {
-    const element = document.getElementById(element_id);
-    element?.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-      inline: "nearest",
-    });
-  };
+  // const scrolltoHash = function (element_id: string) {
+  //   const element = document.getElementById(element_id);
+  //   element?.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "end",
+  //     inline: "nearest",
+  //   });
+  // };
   return (
     <>
       <Head>
@@ -34,7 +31,6 @@ export default function Home() {
         <Video />
         <Prices />
         <AgroVideo />
-        <PartnersBlock />
       </Layout>
     </>
   );
